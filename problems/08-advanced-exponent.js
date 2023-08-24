0cb6b50d82bb12ae14dc72fff74c6102e9c7004e
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2023-08-24 00:59:47 -0700

    Problemo numero ocho

--- problems/08-advanced-exponent.js
+++ problems/08-advanced-exponent.js
@@ -20,31 +20,40 @@ exponentiation in your solution.
 
 Examples:
 
-advancedExponent(2, 0); // 1
-advancedExponent(2, 1); // 2
-advancedExponent(2, 2); // 4
-advancedExponent(2, 3); // 8
-advancedExponent(2, 4); // 16
-advancedExponent(2, 5); // 32
-advancedExponent(2, 6); // 64
-advancedExponent(2, 7); // 128
-advancedExponent(2, 8); // 256
-advancedExponent(2, 9); // 512
-advancedExponent(2, 10); // 1024
-advancedExponent(2, 11); // 2048
-advancedExponent(2, 12); // 4096
+
 
 For each of the examples above, figure out how many times your code should
 be recursively calling `advancedExponent`. Find a way to visually see how many
 times `advancedExponent` is being recursively called.
 ***********************************************************************/
 
-
+let recursiveCalls = 0
 function advancedExponent(b, n) {
-  // Your code here 
+  recursiveCalls++;
+  if (n === 0) return 1;
+  if (n === 1) return b;
+  if (n % 2 === 0) {
+    let halfPower = advancedExponent(b, n / 2);
+    return halfPower ** 2
+  } else {
+    let oddHalfPower = advancedExponent(b, (n - 1) / 2);
+    return b * oddHalfPower ** 2
+  }
 }
 
-
+console.log(advancedExponent(2, 0), 1); // 1
+console.log(advancedExponent(2, 1), 2); // 2
+console.log(advancedExponent(2, 2), 3); // 4
+console.log(advancedExponent(2, 3), 4); // 8
+console.log(advancedExponent(2, 4), 5); // 16
+console.log(advancedExponent(2, 5), 6); // 32
+console.log(advancedExponent(2, 6), 7); // 64
+console.log(advancedExponent(2, 7), 8); // 128
+console.log(advancedExponent(2, 8), 9); // 256
+console.log(advancedExponent(2, 9), 10); // 512
+console.log(advancedExponent(2, 10), 11); // 1024
+console.log(advancedExponent(2, 11), 12); // 2048
+console.log(advancedExponent(2, 12), 13); // 4096
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = advancedExponent;
